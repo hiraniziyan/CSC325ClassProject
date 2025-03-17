2e9dbd23b537a43b5bfe28b9128383a25894a681
hiraniziyan <141653186+hiraniziyan@users.noreply.github.com>
2025-03-17 22:23:56 +0000

    changed layout and page.tsx

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -15,10 +15,12 @@ export default function RootLayout(props: { children: React.ReactNode }) {
       <body>
         <InitColorSchemeScript attribute="class" />
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <AppTheme {...props}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <AppTheme {...props}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline enableColorScheme />
+
             <AppAppBar />
+           
             {props.children}
           </AppTheme>
         </AppRouterCacheProvider>
